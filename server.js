42e40f7d9d51dd071a89818c8693bb26555d5b10
Evan Mackay <evanmackay@Evans-MacBook-Pro.local>
2020-10-22 23:05:21 -0500

    fixed file structure

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(express.static("public"));
 app.use("/api/servicemembers", apiRoutes)
 
 
-db.sequelize.sync( { force: true }).then(function() {
+
+db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT); 
     });
