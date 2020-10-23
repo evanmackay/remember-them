4029525b1117d1014a85b1e6472196ef9332389e
Jon Jessee <tjessee7624@gmail.com>
2020-10-23 16:30:59 -0600

    Set up views and layouts directory, imported handlebars, and created boilerplate for main.handlebars.

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 var express = require("express")
-
 var app = express();
 var PORT = process.env.PORT || 8080;
 var apiRoutes = require("./routes/api-routes.js")
 var db = require("./models");
+const expbhs = require('express-handlebars');
 
+//Set handlebars as vewing engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Middleware for data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Contents of public folder are now staticly served
 app.use(express.static("public"));
 
 // require("./routes/api-routes.js")(app)
 app.use("/api/servicemembers", apiRoutes)
 
-
-db.sequelize.sync( { force: true }).then(function() {
+//Sync sequelize models and start the express app
+db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT); 
     });
