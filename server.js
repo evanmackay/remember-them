2204df8885c48263b226a27a75b2e604e894a228
Evan Mackay <evanmackay@Evans-MacBook-Pro.local>
2020-10-23 19:28:33 -0500

    fixed some typos and added express to package.json

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 var apiRoutes = require("./routes/api-routes.js")
 var db = require("./models");
-const expbhs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 //Set handlebars as vewing engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -19,12 +19,8 @@ app.use(express.static("public"));
 // require("./routes/api-routes.js")(app)
 app.use("/api/servicemembers", apiRoutes)
 
-<<<<<<< HEAD
 
 
-=======
-//Sync sequelize models and start the express app
->>>>>>> 24ea9290f8a3b40753c85b50d7a86760d1a4327c
 db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT); 
