a417ac76bb16564fb39770dfbbc4fcb11d1e0d9d
cleminator <alvinclemens@gmail.com>
2020-10-25 20:38:32 -0600

    building the swiper

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,7 @@ const sequilize = require('sequelize')
 const Sequilize = new sequilize (
     'seals_db',
     'seals_db_root@localhost',
-    'seals_db_Toskiornottoski500!'
+    'seals_db_Toskiornottoski500!',
     {
         dialect:'mysql',
         host: 'localhost'
