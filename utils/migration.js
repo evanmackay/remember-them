6ea02f19a2b8386ed3e3e2b2f91f94c538b3aed9
cleminator <alvinclemens@gmail.com>
2020-11-02 16:19:06 -0700

    the data is storted in a csv file and now needs to be worked into our sequilize db to be run

--- utils/migration.js
+++ utils/migration.js
@@ -1,32 +1,42 @@
 const db = require("../models")
 const fs = require("fs")
-const express = require("express");
-const router = express.Router();
-db.sequelize.sync().then(function () {
+const fastcsv = require("fast-csv");
+
+
+
 var sealStingData = fs.readFileSync(__dirname + "\\seals_db.csv").toString().split("\n")
+let csvData = [];
+let csvStream = fastcsv
+  .parse()
+  .on("data", function(data) {
+    csvData.push(data);
+  })
+  .on("end", function() {
+    // remove the first line: header
+    csvData.shift();
 
-// console.log(sealStingData)
+    // create a new connection to the database
+    const connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "Toskiornottoski500!",
+      database: "servicemembers"
+    });
 
-for (i = 1; i < sealStingData.length; i++) {
- let curent = sealStingData[i].split(",")
-//  let  = sealStingData[i].split(" ")
-let img = sealStingData[i].split("")
- console.log(curent)
-}
-router.post("/SEALs", function (req, res) {
+    // open the connection
+    connection.connect(error => {
+      if (error) {
+        console.error(error);
+      } else {
+        let query =
+          "INSERT INTO servicemembers (id, first_name, last_name, age, branch_of_service, unit, date_of_death,awards, summary_of_service) VALUES ?";
+        connection.query(query, [csvData], (error, response) => {
+          console.log(error || response);
+        });
+      }
+    });
+  });
 
-    db.ServiceMember.create({
-        
-        first_name:req.body.curent,
-        last_name:"johnson",
-        age:33,
-        branch_of_service:"fjiewojiofew",
-        unit:"fiwoejoihwejfiohu",
-        date_of_death:new Date (),
-        awards:"wefuihwef",
-        summary_of_service:"iuhefwuihwef",
-        approved:true
-    })
-    
-});
-})
+  sealStingData.push(csvStream);
+  console.log(sealStingData)
+
