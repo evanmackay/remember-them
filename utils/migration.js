d3873791ad2c9a12cdf7223680711a0a832c7a30
cleminator <alvinclemens@gmail.com>
2020-11-02 15:34:58 -0700

    new database working it from the csv file

--- utils/migration.js
+++ utils/migration.js
@@ -1,7 +1,7 @@
 const db = require("../models")
 const fs = require("fs")
-
-
+const express = require("express");
+const router = express.Router();
 db.sequelize.sync().then(function () {
 var sealStingData = fs.readFileSync(__dirname + "\\seals_db.csv").toString().split("\n")
 
@@ -10,13 +10,15 @@ var sealStingData = fs.readFileSync(__dirname + "\\seals_db.csv").toString().spl
 for (i = 1; i < sealStingData.length; i++) {
  let curent = sealStingData[i].split(",")
 //  let  = sealStingData[i].split(" ")
+let img = sealStingData[i].split("")
  console.log(curent)
 }
-  
+router.post("/SEALs", function (req, res) {
+
     db.ServiceMember.create({
         
-        first_name: curent,
-        last_name:"smith",
+        first_name:req.body.curent,
+        last_name:"johnson",
         age:33,
         branch_of_service:"fjiewojiofew",
         unit:"fiwoejoihwejfiohu",
@@ -27,3 +29,4 @@ for (i = 1; i < sealStingData.length; i++) {
     })
     
 });
+})
