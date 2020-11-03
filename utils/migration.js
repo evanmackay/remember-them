892c5476154be636dbad4d28a4c83fd530a6873a
cleminator <alvinclemens@gmail.com>
2020-11-02 18:31:39 -0700

    bad code

--- utils/migration.js
+++ utils/migration.js
@@ -1,42 +1,40 @@
 const db = require("../models")
 const fs = require("fs")
-const fastcsv = require("fast-csv");
 
 
+db.sequelize.sync().then(function () {
+    var sealStingData = fs.readFileSync(__dirname + "\\seals_db.csv").toString().split("\n")
 
-var sealStingData = fs.readFileSync(__dirname + "\\seals_db.csv").toString().split("\n")
-let csvData = [];
-let csvStream = fastcsv
-  .parse()
-  .on("data", function(data) {
-    csvData.push(data);
-  })
-  .on("end", function() {
-    // remove the first line: header
-    csvData.shift();
+    // console.log(sealStingData)
 
-    // create a new connection to the database
-    const connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "Toskiornottoski500!",
-      database: "servicemembers"
+    for (i = 1; i < sealStingData.length; i++) {
+        // the data is in their corrisponding rows
+        let current = sealStingData[i].split(",")
+
+        //  let  = sealStingData[i].split(" ")
+        // let img = sealStingData[i].split("https://")
+        console.log(current)
+    }
+    router.post("/SEALs", function (req, res) {
+        db.ServiceMember.create({
+
+            first_name: req.res.name,
+            last_name: "smith",
+            age: 33,
+            branch_of_service: "navy",
+            unit: "",
+            date_of_death: new Date(),
+            awards: "wefuihwef",
+            summary_of_service: "iuhefwuihwef",
+            approved: true
+        })
+            .then(function (dbServiceMember) {
+                res.json(dbServiceMember)
+
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     });
 
-    // open the connection
-    connection.connect(error => {
-      if (error) {
-        console.error(error);
-      } else {
-        let query =
-          "INSERT INTO servicemembers (id, first_name, last_name, age, branch_of_service, unit, date_of_death,awards, summary_of_service) VALUES ?";
-        connection.query(query, [csvData], (error, response) => {
-          console.log(error || response);
-        });
-      }
-    });
-  });
-
-  sealStingData.push(csvStream);
-  console.log(sealStingData)
-
+});
