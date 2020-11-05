2791cc49ad3b0b44de463981b26bd5c726c1271f
Evan Mackay <evanmackay@Evans-MacBook-Pro.local>
2020-11-04 18:31:24 -0600

    saving locallay

---
+++ models/forum.js
@@ -0,0 +1,6 @@
+// module.exports = (function(sequelize, DataTypes) {
+//     var ForumData = sequelize.define("Post", {
+//         text: {type: DataTypes.TEXT, allowNull: false, validate: {len: [1,1000] } },
+//         name: {type: DataTypes.STRING, allowNull: false, validate {len: [1, 100] } }
+//     })
+// })
